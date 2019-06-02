4d2d26776661ee8fea234ca429b586e231efdd33
Alex Zhukovich <zhukovich.a@gmail.com>
2019-06-02 11:59:22 +0200

    Renamed layout from activity_main to activity_movies

--- app/src/main/java/com/alexzh/popularmovies/movies/MoviesActivity.kt
+++ app/src/main/java/com/alexzh/popularmovies/movies/MoviesActivity.kt
@@ -8,6 +8,6 @@ class MoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
     }
 }
